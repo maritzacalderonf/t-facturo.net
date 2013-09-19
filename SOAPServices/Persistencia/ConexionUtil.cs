4185cbccd087ebc43a13b8f1d4c30f2b973ee877
Maritza Calderón <maritzacalderonf@gmail.com>
2013-09-19 13:24:22 -0500

    3

--- SOAPServices/Persistencia/ConexionUtil.cs
+++ SOAPServices/Persistencia/ConexionUtil.cs
@@ -10,7 +10,7 @@ namespace SOAPServices.Persistencia
         public static string ObtenerCadena() 
         {
             return "Data Source=acce5e80-bbc4-4000-900a-a23900003d8c.sqlserver.sequelizer.com;Initial Catalog=dbacce5e80bbc44000900aa23900003d8c; User  Id=kzehosethznirtsk; Password=f2XSjHhbfDWspJWeRrVcEtpTyb52A7CoGtTvPDpYVhnd42DPhBKx7P2ULeshMpHh;";
-        //sdfs
+        
         }
     }
 }
